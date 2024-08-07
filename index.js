cb716eee7bda80504068b2fed39134e095f5b75e
kartikvermaa <91695163+kartikvermaa@users.noreply.github.com>
2024-08-07 12:35:51 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const myServer = http.createServer((req,res) =>{
+    console.log("New Request Recived!!");
+    console.log("hello from ec2 changed")
+    res.end("NOW WE ARE ON EC2 Done Successfully yyyyyy");
+    
+});
+
+myServer.listen(9000, ()=> console.log("Server Started on port No. 9000"));
