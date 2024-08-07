98badb0782c5fc596e95433f67da320d28ae6521
kartikvermaa <91695163+kartikvermaa@users.noreply.github.com>
2024-08-07 13:36:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const http = require("http");
 
 const myServer = http.createServer((req,res) =>{
     console.log("New Request Recived!!");
-    console.log("hello from ec2 changed")
+    console.log("hello from ec2 changed");
+    console.log("hiiii")
     res.end("NOW WE ARE ON EC2 Done Successfully flask");
     
 });
