0ee37e264f9bd0547f924fb2c1748fdaa44d81ae
kartikvermaa <91695163+kartikvermaa@users.noreply.github.com>
2024-08-07 16:09:56 +0530

    apple index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const myServer = http.createServer((req,res) =>{
     console.log("New Request Recived!!");
-    console.log("hello from ec2 changed");
+    console.log("hello from ec2 Apple");
     console.log("hiiii from pyy")
     res.end("NOW WE ARE ON EC2 Done Successfully flask");
     
