01ee1dbdbae72dc2d96e1ec7d410a3cefdca81c1
kartikvermaa <91695163+kartikvermaa@users.noreply.github.com>
2024-08-27 15:02:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const myServer = http.createServer((req,res) =>{
     console.log("New  ..  changed Request Recived!!!!");
     console.log("its working, it is latest code  new ");
     console.log("new push done  ")
-    res.end(" NOW WE ARE ON EC2 Done Successfully !!");
+    res.end(" Hey Kartik ! NOW WE ARE ON EC2 Successfully !!");
     
 });
 
