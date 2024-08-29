8b0e4f844eff313ed7a32f2a69273b23cfa6c8cc
kartikvermaa <91695163+kartikvermaa@users.noreply.github.com>
2024-08-29 14:46:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const myServer = http.createServer((req,res) =>{
     console.log("New changed Request Recived!!");
     console.log("its working, it is latest code new edit ");  
 
-    res.end(" Hey Kartik WE ARE ON EC2 Successfully");
+    res.end(" Hey WE ARE ON EC2 AND CONTAINERIZATION IS Successfully");
     
 });
 
