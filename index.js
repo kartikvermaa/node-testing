e7a0221c0fce76b45720fed4519160af6d00b589
kartikvermaa <91695163+kartikvermaa@users.noreply.github.com>
2024-09-06 12:59:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const myServer = http.createServer((req,res) =>{
     console.log("New changed Request Recived!!");
     console.log("its working, it is latest code new edit ");  
 
-    res.end(" hey Kartik       Task completed.");
+   res.end(`Welcome to my project presentation!
+This project showcases a fully automated CI/CD pipeline for deploying a Node.js application using GitHub Webhooks, Docker, and PM2.
+With every code push to the main branch, the pipeline rebuilds the Docker image, updates the application, and restarts it seamlessly.
+Technologies used include:
+- Node.js for backend
+- Docker for containerization
+- PM2 for process management
+- GitHub Webhooks for continuous integration
+
+Thank you for your time!`);
+
     
     
 });
